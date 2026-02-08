614491c9b4ad9878b8e8b484c69d36f0e437eda8
safarovyusif <safarov.yusif@yahoo.com>
2026-02-08 19:37:08 +0000

    Fix tasks 0 and 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,9 +1,8 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  return [room1, room2, room3];
 }
