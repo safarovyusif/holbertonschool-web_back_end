d4d543f5b54b7ebaa264a76467ef40c7c6aa0576
safarovyusif <safarov.yusif@yahoo.com>
2026-02-22 19:14:48 +0000

    task

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Hazırda bizdən sadəcə Promise qaytarmaq tələb olunur, 
+    // ona görə də içi boş ola bilər və ya birbaşa resolve() edə bilərik.
+  });
+}
